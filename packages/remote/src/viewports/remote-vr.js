0d702c7a76524644f8c0f22b75ca621a8adcc2c8
zhenlang.sun <szhenliang@infervision.com>
2022-03-22 18:42:19 +0800

    兼容服务端渲染VR 中paraview 的bug

--- packages/remote/src/viewports/remote-vr.js
+++ packages/remote/src/viewports/remote-vr.js
@@ -3,22 +3,33 @@ import AbstractRemoteStreamViewport from "./base/abstract-remote-stream";
 class RemoteVRViewport extends AbstractRemoteStreamViewport {
   constructor(options = {}) {
     super(options);
-    // paraview 的bug，导致右键可能不派发数据。 所以每次enter进来的时候，强行触发一次鼠标抬起
-    // const { el } = options;
-    // el.addEventListener("mouseenter", (e) => {
-    //   this?.stopMouseInteraction();
-    // });
+    // paraview 的bug，导致右键可能不派发数据。 强行触发一次鼠标抬起
+    const { el } = options;
+    const dropHandler = (e) => {
+      // let { offsetX, offsetY, currentTarget } = e;
+      // const { clientWidth, clientHeight } = currentTarget;
+      // let mouse = { x: offsetX / clientWidth, y: 1 - offsetY / clientHeight };
+      // // 可能存在宽高为0的情况？
+      // if (clientWidth === 0 || clientHeight === 0) {
+      //   mouse = { x: 0.5, y: 0.5 };
+      // }
+      // this?.stopMouseInteraction(mouse);
+      if (this?.remoteRenderer?.mouseHandler) {
+        this.remoteRenderer.mouseHandler.inRightClickHandling = false;
+      }
+    };
 
-    // el.addEventListener("mouseup", (e) => {
-    //   console.log(this.remoteRenderer, e);
-    //   let { offsetX, offsetY, clientWidth, clientHeight } = e;
-    //   let mouse = { x: offsetX / clientWidth, y: 1 - offsetY / clientHeight };
-    //   // 可能存在宽高为0的情况？
-    //   if (clientWidth === 0 || clientHeight === 0) {
-    //     mouse = { x: 0.5, y: 0.5 };
-    //   }
-    //   this?.stopMouseInteraction(mouse);
-    // });
+    el.addEventListener("mouseout", (e) => {
+      if (e.buttons === 2) {
+        dropHandler(e);
+      }
+    });
+
+    el.addEventListener("mouseup", (e) => {
+      if (e.button === 2) {
+        dropHandler(e);
+      }
+    });
   }
 
   async initialAsyncWorkflow() {
