93ad79e54c9a85d42c4c4810a46f0aedfeacbb87
zhenlang.sun <szhenliang@infervision.com>
2022-03-24 15:24:12 +0800

    冠脉vr_tree的文字mapping控制接口

--- packages/remote/src/msg/method-vr_tree.js
+++ packages/remote/src/msg/method-vr_tree.js
@@ -1,1 +1,12 @@
-export const METHODS = {};
+import { MsgTypes } from "./msgTypes";
+export const METHODS = {
+  setVesselNameMapping: async function (mapping) {
+    const [route, method] = this._splitMessageType(MsgTypes.VR_VESSEL_NAME_MAPPING);
+    const session = this.connection.getSession();
+    const data = await session.call(method, [], {
+      vessel_mapping: mapping,
+    });
+
+    return data;
+  },
+};
